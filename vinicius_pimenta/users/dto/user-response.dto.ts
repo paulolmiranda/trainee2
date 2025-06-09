4a9698fdf07349fc87a78ceb2f340751ff18e1ba
vinicius1978pimenta <144499204+vinicius1978pimenta@users.noreply.github.com>
2025-06-09 17:15:25 -0300

    feat/adicionando o mapping

---
+++ vinicius_pimenta/users/dto/user-response.dto.ts
@@ -0,0 +1,6 @@
+import { OmitType } from '@nestjs/mapped-types';
+import { User } from '../entities/user.entity';
+
+export class UserResponseDto extends OmitType(User, ['password'] as const) {
+ 
+}
