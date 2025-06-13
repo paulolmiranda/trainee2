85ee2061b3dc68550a4ea5382ef258a518f5fbbe
ThiagoOMiranda <th_profile@hotmail.com>
2025-06-13 19:48:32 -0300

    fix(tarefa-5) Remove os arquivos da tarefa-4 que subiram no commit da tarefa-5.

--- thiago_miranda/tarefa_4/src/product/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './product.entity';
-export * from './product.module';
-export * from './product.service';
-export * from './product.repository';
-export * from './product.controller';
--- thiago_miranda/tarefa_4/src/shared/baseEntity.ts
+++
@@ -1,9 +0,0 @@
-import { v4 as uuidv4 } from 'uuid';
-
-export abstract class BaseEntity {
-  id: string;
-
-  constructor(partial?: Partial<BaseEntity>) {
-    this.id = partial?.id || uuidv4();
-  }
-}
